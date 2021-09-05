7e70bc903f01099c8e52ed4418e8c7cdafd960c5
mycro8441 <80694805+mycro8441@users.noreply.github.com>
2021-09-05 23:50:41 +0900

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,5 +1,14 @@
 const images = [
-
+    "images/1.jpg",
+    "images/2.jpg",
+    "images/3.jpg",
+    "images/4.jpg",
+    "images/5.jpg",
+    "images/6.jpg",
+    "images/7.jpg",
+    "images/8.jpg",
+    "images/9.jpg",
+    "images/10.jpg",
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
