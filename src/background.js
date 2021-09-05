c6e8d8144430aabb3182dacac5462139f8ab4337
mycro8441 <80694805+mycro8441@users.noreply.github.com>
2021-09-05 23:44:15 +0900

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,1 +1,8 @@
+const images = [
 
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+console.log(chosenImage);
+
+document.body.style.background = `url(${chosenImage})`;
